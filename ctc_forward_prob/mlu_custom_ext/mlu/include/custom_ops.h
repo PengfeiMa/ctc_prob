0989cb7e69251b93fb96efa99e7dfa5936444fb1
mapengfei <mapengfei@cambricon.com>
2023-10-19 17:48:36 +0800

    super core

--- ctc_forward_prob/mlu_custom_ext/mlu/include/custom_ops.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <pybind11/pybind11.h>
-#include <torch/extension.h>
-
-void ctc_forward_prob_mlu(torch::Tensor r, torch::Tensor log_phi, torch::Tensor x_, int start, int end);
--- yd_conformer_plugin/README.md
+++
@@ -1,3 +0,0 @@
-
-# 编译Plugin库
-使用 ./build.sh 编译MagicMind pulginop， 生成路径为 build/lib
